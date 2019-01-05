dfbfc22297bfc36b413e7696ffd25d3df337eecd
Jamie A. Powell <jamie.drkmtrz@gmail.com>
2019-01-04 22:32:21 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Who Is My Rep</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+
+</head>
+
+
+
+<body>
+
+  <section class="center">
+    <h1 style="font-size:3rem;color: white;" class="heading-animate-top">Who is my Rep?</h1>
+    </section>
+    <hr style="margin:auto;width:40%;border-color:#2d3436;"> 
+
+	<section class="form-one">
+		<form action="search-results.php" method="post">
+		<label for="zipcode"> Input Zipcode</label>
+
+		
+		<input id="zipcode" type="number" name="zip" size="5">
+		
+
+		<input type="submit" name="">
+
+
+
+	</form>
+	</section>
+  
+ 
+
+  <footer>
+    &#169;	 Jamie Powell 2018 
+  </footer>
+
+
+</body>
+</html>
---
+++ search-results.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+<?php
+
+file_get_contents('https://whoismyrepresentative.com/getall_mems.php?zip=&output=json'.$_GET['zip'].'php?zip=&output=json');
+
+
+/*echo $_GET["https://whoismyrepresentative.com/getall_mems.php?zip=&output=json"];*/
+
+
+?>
+
+</body>
+</html>
